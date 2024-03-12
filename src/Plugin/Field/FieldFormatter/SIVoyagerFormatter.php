e93566cf20edd20696f6f5f5f7fa86ffe9b0fb39
Sean Montague <sean.montague@portlandwebworks.com>
2024-03-12 14:32:00 -0400

    3D Scene JPG

--- src/Plugin/Field/FieldFormatter/SIVoyagerFormatter.php
+++ src/Plugin/Field/FieldFormatter/SIVoyagerFormatter.php
@@ -25,23 +25,24 @@ class SIVoyagerFormatter extends FormatterBase {
     public function viewElements(FieldItemListInterface $items, $langcode) {
         $elements = [];
 
-        // Test if the entity is embedded and being edited in CKEDITOR
-        if (isset($_GET['text'])) {
-            $no_thumbnail_url = base_path() . 'core/modules/media/images/icons/no-thumbnail.png';
-        } else {
-            $no_thumbnail_url = NULL;
-        }
-
         foreach ($items as $delta => $item) {
             // Prepare the content or any data you want to pass to the template.
             // Use your theme hook, and pass variables to the template.
+
+            // Test if the entity is embedded and being edited in CKEDITOR
+            if (isset($_GET['text'])) {
+//                ray($item->value . '/scene-image-thumb.jpg');
+                $scene_image = $item->value . '/scene-image-thumb.jpg';
+            } else {
+                $scene_image = NULL;
+            }
 
             $uuid = SiVoyagerUtility::extractUuidFromUrl($item->value);
 
             $elements[$delta] = [
                 '#theme' => 'si_voyager',
                 '#si_voyager_id' => $uuid,
-                '#no_thumbnail_url' => $no_thumbnail_url,
+                '#scene_image' => $scene_image,
                 '#attached' => [
                     'library' => [
                         'si_voyager/si_voyager_viewer',
