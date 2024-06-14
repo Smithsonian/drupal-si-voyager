7412532972ecadbe69575ac38d74dface79b95b7
ericpugh <pugh.eric@gmail.com>
2024-06-14 16:09:50 -0700

    Add field formatter settings, update libraries, css, and template of the viewer #2

--- src/Plugin/Field/FieldFormatter/SIVoyagerFormatter.php
+++ src/Plugin/Field/FieldFormatter/SIVoyagerFormatter.php
@@ -5,6 +5,8 @@ namespace Drupal\si_voyager\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\si_voyager\Utility\SiVoyagerUtility;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Template\Attribute;
 
 /**
  * Plugin implementation of the 'si_voyager' formatter.
@@ -19,37 +21,171 @@ use Drupal\si_voyager\Utility\SiVoyagerUtility;
  */
 class SIVoyagerFormatter extends FormatterBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function viewElements(FieldItemListInterface $items, $langcode) {
-        $elements = [];
+  const VOYAGER_BASE_DOCUMENT_URL = 'https://3d-api.si.edu/content/document';
 
-        foreach ($items as $delta => $item) {
-            // Prepare the content or any data you want to pass to the template.
-            // Use your theme hook, and pass variables to the template.
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [$this->t('Displays 3D Voyager embed.')];
+    return $summary;
+  }
 
-            $uuid = SiVoyagerUtility::extractUuidFromUrl($item->value);
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'show_title' => TRUE,
+      'show_logo' => TRUE,
+      'show_menu' => TRUE,
+      'show_help' => TRUE,
+      'bg_style' => '',
+      'bg_color' => '',
+      'aspect_ratio' => 'aspect-widescreen',
+    ] + parent::defaultSettings();
+  }
 
-            // Test if the entity is embedded and being edited in CKEDITOR
-            if (isset($_GET['text'])) {
-                $scene_image = 'https://3d-api.si.edu/content/document/3d_package:' . $uuid . '/scene-image-thumb.jpg';
-            } else {
-                $scene_image = NULL;
-            }
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $formatterSettings = $this->getSettings();
+    // Prepare the content or any data you want to pass to the template.
+    // Use your theme hook, and pass variables to the template.
+    foreach ($items as $delta => $item) {
+      $uuid = SiVoyagerUtility::extractUuidFromUrl($item->value);
+      $documentUrl = sprintf('%s/%s/', self::VOYAGER_BASE_DOCUMENT_URL, $uuid);
+      $attributes = new Attribute(
+        [
+          'root' => $documentUrl,
+          'document' => 'document.json',
+          'id' => 'voyager-component-' . $uuid,
+          'class' => ['si-voyager-explorer'],
+        ]
+      );
+      // Set the UI elements to display in the viewer.
+      // See: https://smithsonian.github.io/dpo-voyager/explorer/api/#ui-attributes for more information.
+      $uiElements = ['none'];
+      if ((bool) $formatterSettings['show_title']) {
+        $uiElements[] = 'title';
+      }
+      if ((bool) $formatterSettings['show_logo']) {
+        $uiElements[] = 'logo';
+      }
+      if ((bool) $formatterSettings['show_menu']) {
+        $uiElements[] = 'menu';
+      }
+      if ((bool) $formatterSettings['show_help']) {
+        $uiElements[] = 'help';
+      }
+      $uiMode = implode("|", $uiElements);
+      $attributes->setAttribute('uimode', $uiMode);
 
-            $elements[$delta] = [
-                '#theme' => 'si_voyager',
-                '#si_voyager_id' => $uuid,
-                '#scene_image' => $scene_image,
-                '#attached' => [
-                    'library' => [
-                        'si_voyager/si_voyager_viewer',
-                    ],
-                ],
-            ];
-        }
+      if (!empty($formatterSettings['bg_style'])) {
+        $attributes->setAttribute('bgstyle', $formatterSettings['bg_style']);
+      }
 
-        return $elements;
+      if (!empty($formatterSettings['bg_color'])) {
+        $attributes->setAttribute('bgcolor', trim($formatterSettings['bg_color']));
+      }
+
+      // Set the class for the aspect ratio.
+      if (!empty($formatterSettings['aspect_ratio'])) {
+        $attributes->addClass($formatterSettings['aspect_ratio']);
+      }
+
+      // Test if the entity is embedded and being edited in CKEDITOR
+      if (isset($_GET['text'])) {
+        $scene_image = 'https://3d-api.si.edu/content/document/3d_package:' . $uuid . '/scene-image-thumb.jpg';
+      } else {
+        $scene_image = NULL;
+      }
+
+      $elements[$delta] = [
+          '#theme' => 'si_voyager',
+          '#si_voyager_id' => $uuid,
+          '#scene_image' => $scene_image,
+          '#attributes' => $attributes,
+          '#attached' => [
+              'library' => [
+                  'si_voyager/si_voyager_viewer',
+              ],
+          ],
+      ];
     }
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+    $form['settings_heading'] = [
+      '#type' => 'item',
+      '#title' => $this->t('Viewer settings'),
+      '#description' => $this->t('Elements to display in the viewer. (if available)'),
+    ];
+
+    $form['show_title'] = [
+      '#title' => $this->t('Show scene title'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('show_title'),
+    ];
+
+    $form['show_logo'] = [
+      '#title' => $this->t('Show the SI Voyager logo'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('show_logo'),
+    ];
+
+    $form['show_menu'] = [
+      '#title' => $this->t('Show the viewer menu toolbar.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('show_menu'),
+    ];
+
+    $form['show_help'] = [
+      '#title' => $this->t('Show the help button.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('show_help'),
+    ];
+
+    $form['bg_style'] = [
+      '#title' => $this->t('Background style'),
+      '#type' => 'select',
+      '#options' => [
+        '' => $this->t('Default'),
+        'Solid' => $this->t('Solid'),
+        'LinearGradient' => $this->t('Linear Gradient'),
+        'RadialGradient' => $this->t('Radial Gradient'),
+      ],
+      '#default_value' => $this->getSetting('bg_style'),
+    ];
+
+    $form['bg_color'] = [
+      '#title' => $this->t('Background color'),
+      '#type' => 'textfield',
+      '#description' => $this->t('Enter one (Solid) or two (Gradient) CSS colors (separated by a space). Example: "#74D1EA #FFCD00".'),
+      '#default_value' => $this->getSetting('bg_color'),
+    ];
+
+    $form['aspect_ratio'] = [
+      '#title' => $this->t('Aspect ratio'),
+      '#type' => 'select',
+      '#options' => [
+        'aspect-widescreen' => $this->t('16:9 - Widescreen Video'),
+        'aspect-square' => $this->t('1:1 - Square'),
+        'aspect-fullscreen' => $this->t('4:3 - Fullscreen Video'),
+        'aspect-mobile-portrait' => $this->t('9:16 - Mobile Portrait'),
+      ],
+      '#default_value' => $this->getSetting('aspect_ratio'),
+    ];
+
+    return $form;
+  }
+
 }
--- src/Utility/SiVoyagerUtility.php
+++ src/Utility/SiVoyagerUtility.php
@@ -20,7 +20,7 @@ class SiVoyagerUtility {
         $uuidPattern = '/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/';
 
         if (preg_match($uuidPattern, $path, $matches)) {
-            return $matches[0];
+            return trim($matches[0]);
         }
 
         return null;
